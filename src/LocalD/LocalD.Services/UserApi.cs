656b3eff1f747192d3ccf4b18c350cdb4b11c879
Kaelan Fouwels <kfouwels@outlook.com>
2013-04-14 20:27:42 +0100

    I derped - now with valid responses!

--- src/LocalD/LocalD.Services/UserApi.cs
+++ src/LocalD/LocalD.Services/UserApi.cs
@@ -40,7 +40,7 @@ namespace LocalD.Services
             return x;
         }
 
-        public async Task<NewsResponse> ApiNews(string townId)
+        public async Task<List<NewsResponse>> ApiNews(string townId)
         {
             var x = JsonConvert.DeserializeObject<NewsResponseRootObject>(await HttpGet("news/find" + "?key=" + _apiKey + "&town=" + townId)).response;
             return x;
